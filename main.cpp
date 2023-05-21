584a9882b71d8232dff4abeab02b458461f77a4e
antek314 <antoni.mielcarek@student.put.poznan.pl>
2023-05-21 21:59:43 +0200

    commit od antka

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "moze dziala" << endl;
     cout << "Hello World!" << endl;
     cout << "moze dziala" << endl;
+    cout << "Jak teraz zadziała to wisze ci piwo" << endl;
     return 0;
 }
