51d100642211147f5c8b49db245a0cb07af0f082
antek314 <antoni.mielcarek@student.put.poznan.pl>
2023-05-24 15:23:22 +0200

    Animacja tupania i rzucania i tła

--- main.cpp
+++ main.cpp
@@ -10,20 +10,73 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(900, 900), "DR. Mario");
     window.setFramerateLimit(60);
 
-    // W zmiennej font mamy czcionke, jezeli chcesz polskie znaki to przed tekstem daj 'L'
+    // W zmiennej czcionka mamy czcionke, jezeli chcesz polskie znaki to przed tekstem daj 'L'
     // np. sf::Text text(L"śćżźą", font, 24);
-    sf::Font font;
-    if (!font.loadFromFile("arial.ttf"))
+    sf::Font czcionka;
+    if (!czcionka.loadFromFile("arial.ttf"))
     {
         return EXIT_FAILURE;
     }
+    sf::Clock clock;
 
     int etapGry = 1; // 1 - ekran poczatkowy z wyborem ustawien, 2 - ekran gry, 3 - ekran koncowy z wynikiem
     int poziomTrudnosci=0;
-    bool isDragging = false;
+    bool suwak = false;
+    bool gra = true;
     int value = 1;
-    int pozycjaSliderX = 200;
-    int pozycjaSliderY = 290;
+    int pozycjaSuwakX = 200;
+    int pozycjaSuwakY = 290;
+
+
+    sf::Texture doktor1;
+    if (!doktor1.loadFromFile("doktor1.png")) {
+        std::cerr << "Błąd podczas wczytywania tekstury 1." << std::endl;
+        return -1;
+    }
+
+    sf::Texture doktor2;
+    if (!doktor2.loadFromFile("doktor2.png")) {
+        std::cerr << "Błąd podczas wczytywania tekstury 2." << std::endl;
+        return -1;
+    }
+
+    sf::Texture doktor3;
+    if (!doktor3.loadFromFile("doktor3.png")) {
+        std::cerr << "Błąd podczas wczytywania tekstury 3." << std::endl;
+        return -1;
+    }
+
+    sf::Texture doktor4;
+    if (!doktor4.loadFromFile("doktor4.png")) {
+        std::cerr << "Błąd podczas wczytywania tekstury 4." << std::endl;
+        return -1;
+    }
+
+    sf::Texture doktor5;
+    if (!doktor5.loadFromFile("doktor5.png")) {
+        std::cerr << "Błąd podczas wczytywania tekstury 5." << std::endl;
+        return -1;
+    }
+
+    sf::Sprite doktorNoga;
+    doktorNoga.setTexture(doktor1);
+    doktorNoga.setPosition(380, 60);
+
+    sf::Sprite doktorRzut;
+    doktorRzut.setTexture(doktor3);
+    doktorRzut.setPosition(380, 60);
+
+    sf::Texture tlo1;
+        if (!tlo1.loadFromFile("tlo1.jpg")) {
+            std::cerr << "Could not load texture" << std::endl;
+            return 1;
+        }
+
+    sf::Texture tlo2;
+    if (!tlo2.loadFromFile("tlo2.jpg")) {
+        std::cerr << "Could not load texture" << std::endl;
+        return 1;
+    }
 
     while (window.isOpen()) {
 
@@ -37,34 +90,34 @@ int main() {
                 sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 
                 if (etapGry == 1) {
-                    sf::RectangleShape rectangle1(sf::Vector2f(145.0, 45.0));
-                    rectangle1.setPosition(200.0, 460.0);
+                    sf::RectangleShape prostokat1(sf::Vector2f(145.0, 45.0));
+                    prostokat1.setPosition(200.0, 460.0);
 
-                    sf::RectangleShape rectangle2(sf::Vector2f(170.0, 45.0));
-                    rectangle2.setPosition(345.0, 460.0);
+                    sf::RectangleShape prostokat2(sf::Vector2f(170.0, 45.0));
+                    prostokat2.setPosition(345.0, 460.0);
 
-                    sf::RectangleShape rectangle3(sf::Vector2f(170.0, 45.0));
-                    rectangle3.setPosition(515.0, 460.0);
+                    sf::RectangleShape prostokat3(sf::Vector2f(170.0, 45.0));
+                    prostokat3.setPosition(515.0, 460.0);
 
-                    if (rectangle1.getGlobalBounds().contains(mousePosition)) {
+                    if (prostokat1.getGlobalBounds().contains(mousePosition)) {
                         poziomTrudnosci=1;
                     }
-                    else if (rectangle2.getGlobalBounds().contains(mousePosition)) {
+                    else if (prostokat2.getGlobalBounds().contains(mousePosition)) {
                         poziomTrudnosci=2;
                     }
-                    else if (rectangle3.getGlobalBounds().contains(mousePosition)) {
+                    else if (prostokat3.getGlobalBounds().contains(mousePosition)) {
                         poziomTrudnosci=3;
                     }
                     sf::RectangleShape track(sf::Vector2f(400, 10));
                     track.setPosition(200, 300);
                     sf::RectangleShape slider(sf::Vector2f(20, 40));
-                    slider.setPosition(pozycjaSliderX, pozycjaSliderY);
+                    slider.setPosition(pozycjaSuwakX, pozycjaSuwakY);
                     if (event.mouseButton.button == sf::Mouse::Left)
                     {
-                        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                        if (slider.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePosition)))
+                        sf::Vector2i pozycjaMyszy = sf::Mouse::getPosition(window);
+                        if (slider.getGlobalBounds().contains(static_cast<sf::Vector2f>(pozycjaMyszy)))
                         {
-                            isDragging = true;
+                            suwak = true;
                         }
                     }
                 }
@@ -79,7 +132,7 @@ int main() {
             {
                 if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    isDragging = false;
+                    suwak = false;
                 }
             }
         }
@@ -87,77 +140,86 @@ int main() {
         /* Etap pierwszy, wybór trudności gry: *******************************************************/
         if(etapGry==1)
         {
-            std::vector<std::unique_ptr<sf::Drawable>> shapes;
+
+            std::vector<std::unique_ptr<sf::Drawable>> wszystkieKsztalty;
 
             // Tworzenie kształtów, tekstów i dodawanie ich do wektora:
-            sf::RectangleShape rectangle1(sf::Vector2f(145.0, 45.0));
-            rectangle1.setPosition(200.0, 460.0);
-            rectangle1.setFillColor(sf::Color::Green);
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(rectangle1));
 
-            sf::RectangleShape rectangle2(sf::Vector2f(170.0, 45.0));
-            rectangle2.setPosition(345.0, 460.0);
-            rectangle2.setFillColor(sf::Color::Blue);
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(rectangle2));
+                tlo1.setRepeated(true);
+                sf::Sprite sprite;
+                sprite.setTexture(tlo1);
+                //sprite.setScale(0.3, 0.3);
+                sprite.setTextureRect(sf::IntRect(0, 0, 900, 900));
+                wszystkieKsztalty.emplace_back(std::make_unique<sf::Sprite>(sprite));
 
-            sf::RectangleShape rectangle3(sf::Vector2f(170.0, 45.0));
-            rectangle3.setPosition(515.0, 460.0);
-            rectangle3.setFillColor(sf::Color::Red);
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(rectangle3));
+            sf::RectangleShape prostokat1(sf::Vector2f(145.0, 45.0));
+            prostokat1.setPosition(200.0, 460.0);
+            prostokat1.setFillColor(sf::Color::Green);
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(prostokat1));
+
+            sf::RectangleShape prostokat2(sf::Vector2f(170.0, 45.0));
+            prostokat2.setPosition(345.0, 460.0);
+            prostokat2.setFillColor(sf::Color::Blue);
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(prostokat2));
+
+            sf::RectangleShape prostokat3(sf::Vector2f(170.0, 45.0));
+            prostokat3.setPosition(515.0, 460.0);
+            prostokat3.setFillColor(sf::Color::Red);
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(prostokat3));
 
             sf::RectangleShape track(sf::Vector2f(400, 10));
             track.setFillColor(sf::Color::White);
             track.setPosition(200, 300);
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(track));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(track));
 
             sf::RectangleShape slider(sf::Vector2f(20, 40));
             slider.setFillColor(sf::Color::Red);
-            slider.setPosition(pozycjaSliderX, pozycjaSliderY);
+            slider.setPosition(pozycjaSuwakX, pozycjaSuwakY);
 
-            sf::Text tytul("Dr. Mario", font, 70);
+            sf::Text tytul("Dr. Mario", czcionka, 70);
             tytul.setPosition(250.f, 60.f);
             tytul.setFillColor(sf::Color::Red);
             tytul.setStyle(sf::Text::Style::Bold | sf::Text::Style::Underlined);
             tytul.setOutlineColor(sf::Color::Yellow);
             tytul.setOutlineThickness(5);
-            shapes.emplace_back(std::make_unique<sf::Text>(tytul));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(tytul));
 
-            sf::Text tryb(L"Ilość Wirusów:", font, 30);
+            sf::Text tryb(L"Ilość Wirusów:", czcionka, 30);
             tryb.setPosition(210.f, 230.f);
-            shapes.emplace_back(std::make_unique<sf::Text>(tryb));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(tryb));
 
-            sf::Text szybkosc(L"Tryb gry:", font, 30);
+            sf::Text szybkosc(L"Tryb gry:", czcionka, 30);
             szybkosc.setPosition(210.f, 390.f);
-            shapes.emplace_back(std::make_unique<sf::Text>(szybkosc));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(szybkosc));
 
-            sf::Text tryby(L"1. Tryb łatwy  2. Tryb średni  3. Tryb trudny", font, 24);
+            sf::Text tryby(L"1. Tryb łatwy  2. Tryb średni  3. Tryb trudny", czcionka, 24);
             tryby.setPosition(200.f, 465.f);
-            shapes.emplace_back(std::make_unique<sf::Text>(tryby));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(tryby));
 
             // Wyświetlanie opcji która została wybrana klikając przycisk:
             if(poziomTrudnosci==1)
             {
-                sf::Text poziom(L"Łatwy", font, 25);
+                sf::Text poziom(L"Łatwy", czcionka, 25);
                 poziom.setPosition(440.f, 390.f);
-                shapes.emplace_back(std::make_unique<sf::Text>(poziom));
+                wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(poziom));
             }
 
             if(poziomTrudnosci==2)
             {
-                sf::Text poziom(L"Średni", font, 25);
+                sf::Text poziom(L"Średni", czcionka, 25);
                 poziom.setPosition(440.f, 390.f);
-                shapes.emplace_back(std::make_unique<sf::Text>(poziom));
+                wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(poziom));
             }
 
             if(poziomTrudnosci==3)
             {
-                sf::Text poziom(L"Trudny", font, 25);
+                sf::Text poziom(L"Trudny", czcionka, 25);
                 poziom.setPosition(440.f, 390.f);
-                shapes.emplace_back(std::make_unique<sf::Text>(poziom));
+                wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(poziom));
             }
 
             // Przesuwanie suwakiem jeżeli został kliknięty
-            if (isDragging)
+            if (suwak)
             {
                 sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
                 float mouseX = static_cast<float>(mousePosition.x);
@@ -169,29 +231,29 @@ int main() {
 
                 value = (mouseX - 200) / 21 + 1; // Zmiana pozycji myszy na wartość suwaka (1-20)
                 slider.setPosition(mouseX, 290);
-                pozycjaSliderX=mouseX;
-                pozycjaSliderY=290;
+                pozycjaSuwakX=mouseX;
+                pozycjaSuwakY=290;
             }
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(slider));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(slider));
 
             // Tworzenie kształtów, tekstów i dodawanie ich do wektora c.d.:
-            sf::Text ilosc(std::to_string(value), font, 30);
+            sf::Text ilosc(std::to_string(value), czcionka, 30);
             ilosc.setPosition(460.f, 230.f);
-            shapes.emplace_back(std::make_unique<sf::Text>(ilosc));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(ilosc));
 
             sf::RectangleShape przyciskGraj(sf::Vector2f(170, 80));
             przyciskGraj.setFillColor(sf::Color::Red);
             przyciskGraj.setPosition(550, 590);
-            shapes.emplace_back(std::make_unique<sf::RectangleShape>(przyciskGraj));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::RectangleShape>(przyciskGraj));
 
-            sf::Text graj(L"GRAJ", font, 45);
+            sf::Text graj(L"GRAJ", czcionka, 45);
             graj.setPosition(570.f, 600.f);
             graj.setFillColor(sf::Color::Yellow);
-            shapes.emplace_back(std::make_unique<sf::Text>(graj));
+            wszystkieKsztalty.emplace_back(std::make_unique<sf::Text>(graj));
 
             // Wyswietlanie wszystkiego z wektora:
             window.clear(sf::Color::Black);
-            for(const auto &s : shapes) {
+            for(const auto &s : wszystkieKsztalty) {
                 window.draw(*s);
             }
 
@@ -201,6 +263,54 @@ int main() {
         if(etapGry==2)
         {
             std::vector<std::unique_ptr<sf::Drawable>> shapes;
+
+            tlo2.setRepeated(true);
+            sf::Sprite sprite;
+            sprite.setTexture(tlo2);
+            //sprite.setScale(0.3, 0.3);
+            sprite.setTextureRect(sf::IntRect(0, 0, 900, 900));
+            shapes.emplace_back(std::make_unique<sf::Sprite>(sprite));
+
+            if(gra)
+            {
+                // Czas animacji i opóźnienie między klatkami
+                sf::Time animationTime = sf::seconds(0.6f);
+                sf::Time elapsed = clock.getElapsedTime();
+
+                // Przełączanie między teksturami co określony czas
+                if (elapsed >= animationTime)
+                {
+                    if (doktorRzut.getTexture() == &doktor3)
+                        doktorRzut.setTexture(doktor4);
+
+                    else if (doktorRzut.getTexture() == &doktor5)
+                        doktorRzut.setTexture(doktor3);
+
+                    else
+                        doktorRzut.setTexture(doktor5);
+                    clock.restart();
+                }
+                shapes.emplace_back(std::make_unique<sf::Sprite>(doktorRzut));
+            }
+            else
+            {
+                // Czas animacji i opóźnienie między klatkami
+                sf::Time animationTime = sf::seconds(0.6f);
+                sf::Time elapsed = clock.getElapsedTime();
+
+                // Przełączanie między teksturami co określony czas
+                if (elapsed >= animationTime)
+                {
+                    if (doktorNoga.getTexture() == &doktor1)
+                        doktorNoga.setTexture(doktor2);
+                    else
+                        doktorNoga.setTexture(doktor1);
+                    clock.restart();
+                }
+                shapes.emplace_back(std::make_unique<sf::Sprite>(doktorNoga));
+
+            }
+
             window.clear(sf::Color::Black);
             for(const auto &s : shapes) {
                 window.draw(*s);
