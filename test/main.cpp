eb0f1f50cb407c16e0c3bd4475bffeb4b1aec316
Mateusz Moryson <andreismyslov1234@gmail.com>
2023-06-11 02:15:30 +0200

    dodanie losowego generowania przeciwników

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include "potwory.h"
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -5,7 +6,11 @@
 #include <vector>
 
 
+
+
 using namespace std;
+
+
 
 
 int main() {
@@ -28,18 +33,23 @@ int main() {
     vector<vector<int>> macierz(COLUMNS, vector<int>(ROWS));
 
     // create the window
-    sf::RenderWindow window(sf::VideoMode(900, 900), "Tetris", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(900, 900), "Likaristo", sf::Style::Close);
 
 
     sf::RectangleShape rectangle(sf::Vector2f(CELL_SIZE*SCREEN_RESIZE-1, CELL_SIZE*SCREEN_RESIZE-1));
     rectangle.setFillColor(sf::Color(100, 50, 250));
     //rectangle.setPosition(300, 170);
 
+    // inicjalizacja potworow
+    Potwory potwory(88);
+//    for (auto &potwo : potwory.pozycje)
+//    {
+//        cout << potwo.x << " " << potwo.y << " " << potwo.z << endl;
+//    }
+
+
 
     sf::Clock clock;
-
-
-
 
     window.setFramerateLimit(30);
     // run the program as long as the window is open
@@ -48,11 +58,6 @@ int main() {
 
 
         // check all the window's events that were triggered since the last iteration of the loop
-        sf::Time time1 = clock.getElapsedTime();
-        if (time1.asMilliseconds() > 16.7)
-        {
-
-
         sf::Event event;
         while (window.pollEvent(event)) {
             // "close requested" event: we close the window
@@ -60,67 +65,56 @@ int main() {
                 window.close();
             if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Left) {
-                    pozycja--;
+                    if(pozycja>0)
+                        pozycja--;
                 }
                 if (event.key.code == sf::Keyboard::Right) {
-                    pozycja++;
+                    if(pozycja<6)
+                        pozycja++;
                 }
             }
         }
 
-
-
-
-        // clear the window with black color
         window.clear(sf::Color::Black);
-
-
-        // draw everything here...
-        for (int i=0; i< COLUMNS; i++)
-        {
-            for(int j=0; j < ROWS; j++)
-            {
-
-                rectangle.setPosition(CELL_SIZE * i * 4 + pozX, CELL_SIZE * j *4+pozY);
-                if (event.type == sf::Event::MouseButtonPressed) {
-                    if(event.mouseButton.button == sf::Mouse::Left) {
-
-                        sf::Vector2f mouse_position = window.mapPixelToCoords(sf::Mouse::getPosition(window));
-                        int cell_x = mouse_position.x / CELL_SIZE / SCREEN_RESIZE;
-                        int cell_y = mouse_position.y /100;
-                        //cout << mouse_position.x << " " << mouse_position.y << endl;
-                        if(rectangle.getGlobalBounds().contains(mouse_position))
-                            rectangle.setFillColor(sf::Color(250, 0, 0));
-                        //??
-                        //macierz[cell_x][cell_y] = 1 - macierz[cell_x][cell_y];
-                        }
-                        }
-                window.draw(rectangle);
-                rectangle.setFillColor(sf::Color(100, 50, 250));
-
-            }
-        }
-
-
-
 
         for (int i=0; i< COLUMNS; i++)
         {
             for(int j=0; j < ROWS; j++)
             {
                 sf::Time czas = clock.getElapsedTime();
-//                if(czas.asMilliseconds()>(600))
-//                {
+                if(czas.asMilliseconds()>(600))
+                {
                 spadanie++;
-                //time1 = clock.restart();
+                czas = clock.restart();
                 //spadanie=0;
-                //}
+                }
                 rectangle.setPosition(CELL_SIZE * i * 4 + pozX, CELL_SIZE * j *4+pozY);
                 if((i == pozycja && j==spadanie) || (i == pozycja+1 && j == spadanie))
                 {
                 rectangle.setFillColor(sf::Color(250, 0, 0));
+                //macierz[i][j] = ;
                 }
+                for (auto &potwo : potwory.pozycje)
+                {
 
+                    if((i == potwo.x && j==potwo.y))
+                    {
+                        if(potwo.z == 1.f)
+                        {
+                            rectangle.setFillColor(sf::Color(0, 250, 0));
+                        }
+                        else if(potwo.z == 2.f)
+                        {
+                            rectangle.setFillColor(sf::Color(250, 250, 0));
+                        }
+                        else
+                        {
+                            rectangle.setFillColor(sf::Color(250, 0, 250));
+                        }
+
+                    }
+
+                }
 
                 window.draw(rectangle);
                 rectangle.setFillColor(sf::Color(100, 50, 250));
@@ -128,7 +122,7 @@ int main() {
             }
         }
         window.display();
-        }
+
 
 
     }
---
+++ test/potwory.cpp
@@ -0,0 +1,55 @@
+#include "potwory.h"
+#include <SFML/Window.hpp>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<sf::Vector3f> Potwory::pozycje;
+Potwory::Potwory(int liczba)
+{
+    float ix;
+    float iy;
+    float iz;
+    bool zajete=false;
+    sf::Vector3f poz;
+
+    // Inicjalizacja generatora liczb pseudolosowych
+    random_device rd;
+    mt19937 rng(rd());
+
+    // Utworzenie rozkładu równomiernego dla indeksów
+    // Przedział y nie jest od zera w celu ułatwienia rozgrywki
+    uniform_int_distribution<> x(0,7);
+    uniform_int_distribution<> y(5,15);
+    uniform_int_distribution<> z(1,3);
+
+    for (int i = 0; i<liczba; i++)
+    {
+    do
+        {
+        zajete=false;
+            // Wylosowanie indeksu
+            ix = x(rng);
+            iy = y(rng);
+
+             poz.x = ix;
+             poz.y = iy;
+
+            if (find(Potwory::pozycje.begin(), Potwory::pozycje.end(), poz) != Potwory::pozycje.end())
+                zajete = true;
+
+
+    } while(zajete);
+    Potwory::pozycje.emplace_back(poz);
+    }
+    for (auto &lock : pozycje)
+    {
+        iz = z(rng);
+        lock.z = iz;
+    }
+
+}
